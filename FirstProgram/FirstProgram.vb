d3c67afbc4c0d5c7f4190ce7bf1428fc43ec90fb
rosstimo <rosstimo@isu.edu>
2022-01-11 08:40:48 -0700

    add header

--- FirstProgram/FirstProgram.vb
+++ FirstProgram/FirstProgram.vb
@@ -1,4 +1,11 @@
-﻿Module FirstProgram
+﻿'Tim Rossiter
+'RCET0265
+'S2022
+'My First VB.Net program
+'https://github.com/rosstimo/FirstProgram.git
+
+
+Module FirstProgram
 
     Sub Main()
         Console.WriteLine("this is a string")
